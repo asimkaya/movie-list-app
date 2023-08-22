10ed3599ec9ff6b2923c37663e54d99a99cc526a
Asım Emre Kaya <58777354+asimkaya@users.noreply.github.com>
2023-08-22 21:46:15 +0300

    change: home route eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Movie App - Home' },
+  { path: '**', component: HomeComponent, title: 'Movie App - Home' },
+  { path: '', component: HomeComponent, title: 'Movie App - Home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
