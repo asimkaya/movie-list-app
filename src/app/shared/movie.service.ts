a6e8e3efe1e8e7c48355a14249a31629aa439c27
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2023-08-23 13:36:44 +0300

    feat: movie service eklendi ve hata yakalama olusturuldu

---
+++ src/app/shared/movie.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movie } from './models/movie.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  GetMovies(): Observable<any> {
+    return this.http.get<Movie>(environment.apiUrl + '/movies').pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  //Hata Yakalama
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Try again.'));
+  }
+}
+
+
