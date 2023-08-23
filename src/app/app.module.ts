7eb4aa39a1fa60bffe3950caf535b5451b693dca
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2023-08-23 13:00:42 +0300

    feat: json-server module eklendi ve configure edildi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { ComponentModule } from './components/component.module';
 import { PageModule } from './pages/page.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { PageModule } from './pages/page.module';
     BrowserAnimationsModule,
     MatButtonModule,
     ComponentModule,
-    PageModule
+    PageModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
