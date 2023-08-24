542071fbfcac9dda6a915fa623e4b91a63ab54ea
Asım Emre Kaya <asim.emre.kaya@gmail.com>
2023-08-24 14:04:59 +0300

    change: loader home tarafina eklendi. home kartlar grid layout'a gecildi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/shared/models/movie.model';
+import { MovieService } from 'src/app/shared/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies!: Movie[];
+  loading: boolean = true;
+
+  constructor(private api: MovieService) { }
+
+  ngOnInit(): void {
+    this.getAllMovies();
+  }
+
+  getAllMovies() {
+    this.api.GetMovies().subscribe({
+      next: async (data: Movie[]) => {
+        console.log(data);
+        this.movies = await data;
+        this.loading = await false;
+      }, error: (error) => {
+        console.warn(error);
+      }
+    })
+  }
 
 }
